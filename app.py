dc5ce83a7c07000d433582df9ed1ba470d73237f
pankaj4570 <58369700+pankaj4570@users.noreply.github.com>
2020-04-22 23:43:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,6 @@ def predict():
     return render_template('result.html', prediction = my_prediction)
 
 if __name__== '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader= False)
 
 
